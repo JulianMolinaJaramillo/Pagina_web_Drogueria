8d629c9658c825d0b3f8f05e229fe6a8620c616a
JuliánMolinaJaramillo <judmolin@gmail.com>
2023-09-29 14:12:36 -0500

    Se agrega el proyecto

---
+++ Comportamientos.js
@@ -0,0 +1,175 @@
+
+const ColorActual = document.getElementsByClassName("containerCinco")[0];
+const ColorActual1 = document.getElementsByClassName("itemBienvenido")[0];
+const ColorActual2 = document.getElementsByClassName("itemLogin")[0];
+const ColorActual3 = document.getElementsByClassName("itemLoginBack")[0];
+const ColorActual4 = document.getElementsByClassName("itemBuscarBack")[0];
+const ColorActual5 = document.getElementsByClassName("containerCentro")[0];
+const ColorActual6 = document.getElementsByClassName("item1")[0];
+const ColorActual7 = document.getElementsByClassName("item1")[1];
+const ImagenActual = document.getElementsByClassName("itemProducto1")[0];
+const ImagenActual1 = document.getElementsByClassName("itemProducto1")[1];
+const ImagenActual2 = document.getElementsByClassName("itemProducto1")[2];
+const ImagenActual3 = document.getElementsByClassName("itemProducto1")[3];
+const ImagenActual4 = document.getElementsByClassName("itemProductoEnlaces")[0];
+const ImagenActual5 = document.getElementsByClassName("itemProductoEnlaces")[1];
+const ImagenActual6 = document.getElementsByClassName("itemProductoEnlaces")[2];
+const ImagenActual7 = document.getElementsByClassName("itemProductoEnlaces")[3];
+
+
+const Boton1 = document.getElementsByClassName("BotonOpciones")[0];
+const Boton2 = document.getElementsByClassName("BotonOpciones1")[0];
+const Boton3 = document.getElementsByClassName("BotonOpciones2")[0];
+const Boton4 = document.getElementsByClassName("BotonOpciones3")[0];
+const Boton5 = document.getElementsByClassName("BotonOpciones4")[0];
+const Boton6 = document.getElementsByClassName("BotonOpciones5")[0];
+const Boton7 = document.getElementsByClassName("BotonLupa")[0];
+
+Boton1.addEventListener('click',(e) =>{
+ColorActual.style.backgroundColor = "#00a8e9";
+ColorActual1.style.backgroundColor = "#00a8e9";
+ColorActual2.style.backgroundColor = "#00a8e9";
+ColorActual3.style.backgroundColor = "#00a8e9";
+ColorActual4.style.backgroundColor = "#00a8e9";
+ColorActual5.style.backgroundColor = "#00a8e9";
+ColorActual6.style.backgroundColor = "#00a8e9";
+ColorActual6.style.borderColor = "black";
+ColorActual7.style.backgroundColor = "#00a8e9";
+ColorActual7.style.borderColor = "black";
+ImagenActual.style.backgroundColor = "#00a8e9";
+ImagenActual1.style.backgroundColor = "#00a8e9";
+ImagenActual2.style.backgroundColor = "#00a8e9";
+ImagenActual3.style.backgroundColor = "#00a8e9";
+ImagenActual4.style.backgroundColor = "#00a8e9";
+ImagenActual5.style.backgroundColor = "#00a8e9";
+ImagenActual6.style.backgroundColor = "#00a8e9";
+ImagenActual7.style.backgroundColor = "#00a8e9";
+}
+)
+
+Boton2.addEventListener('click',(e) =>{
+    ColorActual.style.backgroundColor = "#ff7d96";
+    ColorActual1.style.backgroundColor = "#ff7d96";
+    ColorActual2.style.backgroundColor = "#ff7d96";
+    ColorActual3.style.backgroundColor = "#ff7d96";
+    ColorActual4.style.backgroundColor = "#ff7d96";
+    ColorActual5.style.backgroundColor = "#ff7d96";
+    ColorActual6.style.backgroundColor = "#ff7d96";
+    ColorActual6.style.borderColor = "black";
+    ColorActual7.style.backgroundColor = "#ff7d96";
+    ColorActual7.style.borderColor = "black";
+    ImagenActual.style.backgroundColor = "#ff7d96";
+    ImagenActual1.style.backgroundColor = "#ff7d96";
+    ImagenActual2.style.backgroundColor = "#ff7d96";
+    ImagenActual3.style.backgroundColor = "#ff7d96";
+    ImagenActual4.style.backgroundColor = "#ff7d96";
+    ImagenActual5.style.backgroundColor = "#ff7d96";
+    ImagenActual6.style.backgroundColor = "#ff7d96";
+    ImagenActual7.style.backgroundColor = "#ff7d96";
+    }
+)
+    Boton3.addEventListener('click',(e) =>{
+        ColorActual.style.backgroundColor = "#c68ee8";
+        ColorActual1.style.backgroundColor = "#c68ee8";
+        ColorActual2.style.backgroundColor = "#c68ee8";
+        ColorActual3.style.backgroundColor = "#c68ee8";
+        ColorActual4.style.backgroundColor = "#c68ee8";
+        ColorActual5.style.backgroundColor = "#c68ee8";
+        ColorActual6.style.backgroundColor = "#c68ee8";
+        ColorActual6.style.borderColor = "black";
+        ColorActual7.style.backgroundColor = "#c68ee8";
+        ColorActual7.style.borderColor = "black";
+        ImagenActual.style.backgroundColor = "#c68ee8";
+        ImagenActual1.style.backgroundColor = "#c68ee8";
+        ImagenActual2.style.backgroundColor = "#c68ee8";
+        ImagenActual3.style.backgroundColor = "#c68ee8";
+        ImagenActual4.style.backgroundColor = "#c68ee8";
+        ImagenActual5.style.backgroundColor = "#c68ee8";
+        ImagenActual6.style.backgroundColor = "#c68ee8";
+        ImagenActual7.style.backgroundColor = "#c68ee8";
+        }
+    )
+    Boton4.addEventListener('click',(e) =>{
+        ColorActual.style.backgroundColor = "#f15a24";
+        ColorActual1.style.backgroundColor = "#f15a24";
+        ColorActual2.style.backgroundColor = "#f15a24";
+        ColorActual3.style.backgroundColor = "#f15a24";
+        ColorActual4.style.backgroundColor = "#f15a24";
+        ColorActual5.style.backgroundColor = "#f15a24";
+        ColorActual6.style.backgroundColor = "#f15a24";
+        ColorActual6.style.borderColor = "black";
+        ColorActual7.style.backgroundColor = "#f15a24";
+        ColorActual7.style.borderColor = "black";
+        ImagenActual.style.backgroundColor = "#f15a24";
+        ImagenActual1.style.backgroundColor = "#f15a24";
+        ImagenActual2.style.backgroundColor = "#f15a24";
+        ImagenActual3.style.backgroundColor = "#f15a24";
+        ImagenActual4.style.backgroundColor = "#f15a24";
+        ImagenActual5.style.backgroundColor = "#f15a24";
+        ImagenActual6.style.backgroundColor = "#f15a24";
+        ImagenActual7.style.backgroundColor = "#f15a24";
+        }
+    )
+    Boton5.addEventListener('click',(e) =>{
+        ColorActual.style.backgroundColor = "#22b573";
+        ColorActual1.style.backgroundColor = "#22b573";
+        ColorActual2.style.backgroundColor = "#22b573";
+        ColorActual3.style.backgroundColor = "#22b573";
+        ColorActual4.style.backgroundColor = "#22b573";
+        ColorActual5.style.backgroundColor = "#22b573";
+        ColorActual6.style.backgroundColor = "#22b573";
+        ColorActual6.style.borderColor = "black";
+        ColorActual7.style.backgroundColor = "#22b573";
+        ColorActual7.style.borderColor = "black";
+        ImagenActual.style.backgroundColor = "#22b573";
+        ImagenActual1.style.backgroundColor = "#22b573";
+        ImagenActual2.style.backgroundColor = "#22b573";
+        ImagenActual3.style.backgroundColor = "#22b573";
+        ImagenActual4.style.backgroundColor = "#22b573";
+        ImagenActual5.style.backgroundColor = "#22b573";
+        ImagenActual6.style.backgroundColor = "#22b573";
+        ImagenActual7.style.backgroundColor = "#22b573";
+        }
+    )
+    Boton6.addEventListener('click',(e) =>{
+        ColorActual.style.backgroundColor = "#ed1e79";
+        ColorActual1.style.backgroundColor = "#ed1e79";
+        ColorActual2.style.backgroundColor = "#ed1e79";
+        ColorActual3.style.backgroundColor = "#ed1e79";
+        ColorActual4.style.backgroundColor = "#ed1e79";
+        ColorActual5.style.backgroundColor = "#ed1e79";
+        ColorActual6.style.backgroundColor = "#ed1e79";
+        ColorActual6.style.borderColor = "black";
+        ColorActual7.style.backgroundColor = "#ed1e79";
+        ColorActual7.style.borderColor = "black";
+        ImagenActual.style.backgroundColor = "#ed1e79";
+        ImagenActual1.style.backgroundColor = "#ed1e79";
+        ImagenActual2.style.backgroundColor = "#ed1e79";
+        ImagenActual3.style.backgroundColor = "#ed1e79";
+        ImagenActual4.style.backgroundColor = "#ed1e79";
+        ImagenActual5.style.backgroundColor = "#ed1e79";
+        ImagenActual6.style.backgroundColor = "#ed1e79";
+        ImagenActual7.style.backgroundColor = "#ed1e79";
+        }
+    )
+    Boton7.addEventListener('click',(e) =>{
+        ColorActual.style.backgroundColor = "#E6E9EF";
+        ColorActual1.style.backgroundColor = "white";
+        ColorActual2.style.backgroundColor = "white";
+        ColorActual3.style.backgroundColor = "white";
+        ColorActual4.style.backgroundColor = "white";
+        ColorActual5.style.backgroundColor = "white";
+        ColorActual6.style.backgroundColor = "#E6E9EF";
+        ColorActual6.style.borderColor = "#E6E9EF";
+        ColorActual7.style.backgroundColor = "#E6E9EF";
+        ColorActual7.style.borderColor = "#E6E9EF";
+        ImagenActual.style.backgroundColor = "white";
+        ImagenActual1.style.backgroundColor = "white";
+        ImagenActual2.style.backgroundColor = "white";
+        ImagenActual3.style.backgroundColor = "white";
+        ImagenActual4.style.backgroundColor = "white";
+        ImagenActual5.style.backgroundColor = "white";
+        ImagenActual6.style.backgroundColor = "white";
+        ImagenActual7.style.backgroundColor = "white";
+        }
+);
